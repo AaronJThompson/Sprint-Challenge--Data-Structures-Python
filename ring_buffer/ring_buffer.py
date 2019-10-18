d5bddb1115f07fc28761256fb2c109337eab8e68
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 10:24:11 +0100

    Complete second pass solution with ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,16 +1,16 @@
-from dll_queue import Queue
-
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
-    self.queue = Queue()
+    self.length = 0
 
   def append(self, item):
     self.storage[self.current] = item
     self.current += 1
     self.current = self.current % self.capacity
+    if self.length < self.capacity:
+      self.length += 1
 
   def get(self):
-    return self.storage
+    return self.storage[:self.length]
