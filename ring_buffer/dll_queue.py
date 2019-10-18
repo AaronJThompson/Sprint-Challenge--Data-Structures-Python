dc5c446ff05190c8512ba093f8394682d454cfcf
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 10:05:41 +0100

    Add init method to DLL

--- ring_buffer/dll_queue.py
+++ ring_buffer/dll_queue.py
@@ -2,3 +2,9 @@ import sys
 sys.path.append('../reverse')
 from reverse import Node
 
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+    
