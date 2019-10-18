f5be92835382f3e6899376d383fdc46486202dcc
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 09:53:50 +0100

    Import linked list to ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,13 +1,20 @@
+import sys
+sys.path.append('../reverse')
+from reverse import LinkedList
+
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
-    self.storage = [None]*capacity
+    self.storage = []
 
   def append(self, item):
-    self.storage[self.current] = item
+    if len(self.storage) < self.capacity:
+      self.storage.append(item)
+    else:
+      self.storage[self.current] = item
     self.current += 1
-    self.current = self.current % len(self.storage)
+    self.current = self.current % self.capacity
 
   def get(self):
     return self.storage
