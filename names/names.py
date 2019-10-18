6acc98dd2bd91dccb6fb6ba082a0326a462c1235
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 10:19:26 +0100

    Comment out old solution

--- names/names.py
+++ names/names.py
@@ -1,5 +1,5 @@
 import time
-
+from trie import Trie
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,10 +11,10 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
+""" for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
-            duplicates.append(name_1)
+            duplicates.append(name_1) """
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
