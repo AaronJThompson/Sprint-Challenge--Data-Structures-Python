271e7b6faf6e383c769549336fc28744eebb45c1
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 09:57:10 +0100

    Add linked list to RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,13 +6,11 @@ class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
-    self.storage = []
+    self.storage = [None]*capacity
+    self.list = LinkedList()
 
   def append(self, item):
-    if len(self.storage) < self.capacity:
-      self.storage.append(item)
-    else:
-      self.storage[self.current] = item
+    self.storage[self.current] = item
     self.current += 1
     self.current = self.current % self.capacity
 
