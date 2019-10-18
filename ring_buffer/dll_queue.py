213db0b814d4a97680b50d4502c43b86fe8f1561
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-18 10:09:31 +0100

    Add add_to_head method

--- ring_buffer/dll_queue.py
+++ ring_buffer/dll_queue.py
@@ -8,3 +8,12 @@ class DoublyLinkedList:
         self.tail = node
         self.length = 1 if node is not None else 0
     
+    def __len__(self):
+        return self.length
+
+    def add_to_head(self, value):
+        node = Node(value)
+        node.set_next(self.head)
+        if self.tail is None:
+            self.tail = node
+        self.length += 1
